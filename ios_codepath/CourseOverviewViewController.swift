26936535bfa81f2387e9ab718dfcb6f4d6c044ee
Jules <jules.walter@gmail.com>
2015-08-09 19:55:09 -0700

    course overview

--- ios_codepath/CourseOverviewViewController.swift
+++ ios_codepath/CourseOverviewViewController.swift
@@ -9,7 +9,18 @@
 import UIKit
 
 class CourseOverviewViewController: UIViewController {
-
+    var course: Course!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var tagLineLabel: UILabel!
+    @IBOutlet weak var monthLabel: UILabel!
+    @IBOutlet weak var dayLabel: UILabel!
+    @IBOutlet weak var bodyLabel: UILabel!
+    @IBOutlet weak var numSessionsLabel: UILabel!
+    @IBOutlet weak var sessionLengthLabel: UILabel!
+    @IBOutlet weak var weekdaysLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,7 +32,16 @@ class CourseOverviewViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func didPressApply(sender: UIButton) {
+        let url = NSURL(string: "https://google.com")!
+        UIApplication.sharedApplication().openURL(url)
+        
+    }
+    
 
+    @IBAction func didPressClose(sender: UIButton) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
     /*
     // MARK: - Navigation
 
